bd8140aaeaee619c37c7276467759a7fd9299163
Neil Parthasarthy <neilp1998@gmail.com>
2023-12-08 16:48:26 -0800

    add nightfox colorscheme because nvchad built in themes have really bad syntax hightlighting

--- home/.config/nvim_custom/mappings.lua
+++ home/.config/nvim_custom/mappings.lua
@@ -1,78 +1,77 @@
 local M = {}
 
 M.disabled = {
-  n = {
-    ["<leader>ff"] = "",
-    ["<leader>th"] = "",
-    ["<leader>rn"] = "",
-    ["<leader>w"] = "",
-    ["[d"] = "",
-    ["]d"] = "",
-  },
-  t = {
-    ["<C-x>"] = "",
-  },
+	n = {
+		["<leader>ff"] = "",
+		["<leader>rn"] = "",
+		["<leader>w"] = "",
+		["[d"] = "",
+		["]d"] = "",
+	},
+	t = {
+		["<C-x>"] = "",
+	},
 }
 
 M.AOne = {
-  n = {
-    ["<C-p>"] = { "<cmd> Telescope find_files hidden=true follow=true <CR>", "Telescope files" },
-    ["<C-f>"] = { "<cmd> Telescope live_grep <CR>", "Telescope fuzzy find" },
-    ["<leader>w"] = { "<cmd> w <CR>", "Save file" },
-    ["g["] = { "<cmd> <silent> lua vim.diagnostic.goto_prev({float=false}) <CR>", "Previous diagnostic" },
-    ["g]"] = { "<cmd> <silent> lua vim.diagnostic.goto_next({float=false}) <CR>", "Next diagnostic" },
-    ["<leader>d"] = { "<cmd> w !diff % - <CR>", "Diff unsaved changes" },
-    ["gi"] = {
-      function()
-        vim.lsp.buf.incoming_calls()
-      end,
-      "Incoming calls",
-    },
-    ["go"] = {
-      function()
-        vim.lsp.buf.outgoing_calls()
-      end,
-      "Outgoing calls",
-    },
-    ["gs"] = {
-      function()
-        vim.lsp.buf.document_symbol()
-      end,
-      "Document symbols",
-    },
-    ["gw"] = {
-      function()
-        require("telescope.builtin").lsp_dynamic_workspace_symbols()
-      end,
-      "Workspace symbols",
-    },
-    ["gp"] = {
-      function()
-        require("telescope.builtin").registers()
-      end,
-      "Telescope registers",
-    },
-    ["ge"] = {
-      function()
-        require("telescope.builtin").resume()
-      end,
-      "Telescope resume",
-    },
-  },
+	n = {
+		["<C-p>"] = { "<cmd> Telescope find_files hidden=true follow=true <CR>", "Telescope files" },
+		["<C-f>"] = { "<cmd> Telescope live_grep <CR>", "Telescope fuzzy find" },
+		["<leader>w"] = { "<cmd> w <CR>", "Save file" },
+		["g["] = { "<cmd> <silent> lua vim.diagnostic.goto_prev({float=false}) <CR>", "Previous diagnostic" },
+		["g]"] = { "<cmd> <silent> lua vim.diagnostic.goto_next({float=false}) <CR>", "Next diagnostic" },
+		["<leader>d"] = { "<cmd> w !diff % - <CR>", "Diff unsaved changes" },
+		["gi"] = {
+			function()
+				vim.lsp.buf.incoming_calls()
+			end,
+			"Incoming calls",
+		},
+		["go"] = {
+			function()
+				vim.lsp.buf.outgoing_calls()
+			end,
+			"Outgoing calls",
+		},
+		["gs"] = {
+			function()
+				vim.lsp.buf.document_symbol()
+			end,
+			"Document symbols",
+		},
+		["gw"] = {
+			function()
+				require("telescope.builtin").lsp_dynamic_workspace_symbols()
+			end,
+			"Workspace symbols",
+		},
+		["gp"] = {
+			function()
+				require("telescope.builtin").registers()
+			end,
+			"Telescope registers",
+		},
+		["ge"] = {
+			function()
+				require("telescope.builtin").resume()
+			end,
+			"Telescope resume",
+		},
+	},
 
-  v = {
+	v = {
 
-    ["<leader>fm"] = {
-      function()
-        vim.lsp.buf.format { async = true }
-      end,
-      "LSP formatting",
-    },
-  },
+		["<leader>fm"] = {
+			function()
+				vim.lsp.buf.format({ async = true })
+			end,
+			"LSP formatting",
+		},
+	},
 
-  t = {
-    ["<esc>"] = { vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), "Escape terminal mode" },
-  },
+	t = {
+		["<esc>"] = { vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), "Escape terminal mode" },
+	},
 }
 
 return M
--- home/.config/nvim_custom/plugins.lua
+++ home/.config/nvim_custom/plugins.lua
@@ -1,215 +1,220 @@
 local plugins = {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          local null_ls = require "null-ls"
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					local null_ls = require("null-ls")
 
-          local b = null_ls.builtins
+					local b = null_ls.builtins
 
-          local sources = {
-            b.formatting.stylua,
-            b.formatting.clang_format,
-            b.diagnostics.pylint,
-            b.formatting.yapf.with { extra_args = { "--style", os.getenv "HOME" .. "/.yapfrc" } },
-          }
+					local sources = {
+						b.formatting.stylua,
+						b.formatting.clang_format,
+						b.diagnostics.pylint,
+						b.formatting.yapf.with({ extra_args = { "--style", os.getenv("HOME") .. "/.yapfrc" } }),
+					}
 
-          null_ls.setup {
-            debounce = 150,
-            debug = true,
-            sources = sources,
-          }
-        end,
-      },
-    },
-    config = function()
-      local configs = require "plugins.configs.lspconfig"
+					null_ls.setup({
+						debounce = 150,
+						debug = true,
+						sources = sources,
+					})
+				end,
+			},
+		},
+		config = function()
+			local configs = require("plugins.configs.lspconfig")
 
-      for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
-        require("lspconfig")[server].setup {
-          on_attach = configs.on_attach,
-          capabilities = configs.capabilities,
-        }
-      end
-    end,
-  },
+			for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
+				require("lspconfig")[server].setup({
+					on_attach = configs.on_attach,
+					capabilities = configs.capabilities,
+				})
+			end
+		end,
+	},
 
-  {
-    "williamboman/mason.nvim",
-    dependencies = {
-      {
-        "williamboman/mason-lspconfig.nvim",
-        opts = {
-          ensure_installed = {
-            "pyright",
-            "clangd",
-            "lua_ls",
-            "ocamllsp",
-          },
-        },
-      },
-    },
-    opts = {
-      ensure_installed = {
-        "clang-format",
-        "yapf",
-        "stylua",
-      },
-    },
-  },
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			{
+				"williamboman/mason-lspconfig.nvim",
+				opts = {
+					ensure_installed = {
+						"pyright",
+						"clangd",
+						"lua_ls",
+						"ocamllsp",
+					},
+				},
+			},
+		},
+		opts = {
+			ensure_installed = {
+				"clang-format",
+				"yapf",
+				"stylua",
+			},
+		},
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      { "p00f/nvim-ts-rainbow", lazy = false },
-    },
-    opts = {
-      ensure_installed = { "c", "lua", "rust", "cpp", "python", "vim", "vimdoc" },
-      sync_install = false,
-      highlight = {
-        enable = true,
-        disable = { "rust" },
-        additional_vim_regex_highlighting = false,
-        use_languagetree = true,
-      },
-      indent = {
-        enable = true,
-        -- disable = { "python" },
-      },
-      rainbow = {
-        enable = true,
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			{ "p00f/nvim-ts-rainbow", lazy = false },
+		},
+		opts = {
+			ensure_installed = { "c", "lua", "rust", "cpp", "python", "vim", "vimdoc" },
+			sync_install = false,
+			highlight = {
+				enable = true,
+				disable = { "rust" },
+				additional_vim_regex_highlighting = false,
+				use_languagetree = true,
+			},
+			indent = {
+				enable = true,
+				-- disable = { "python" },
+			},
+			rainbow = {
+				enable = true,
+			},
+		},
+	},
 
-  {
-    "telescope.nvim",
-    dependencies = {
-      "gbrlsnchs/telescope-lsp-handlers.nvim",
-    },
-    opts = function()
-      local conf = require "plugins.configs.telescope"
-      local custom = {
-        file_ignore_patterns = { "/.git/", "^.git/", "^.clangd/", "/.clangd/", "^.cache/", "/.cache/" },
-        pickers = {
-          find_files = {
-            hidden = true,
-            follow = true,
-          },
-        },
-        extensions = {
-          lsp_handlers = {
-            disable = {
-              ["textDocument/declaration"] = true,
-              ["textDocument/definition"] = true,
-              ["textDocument/implementation"] = true,
-              ["textDocument/typeDefinition"] = true,
-              ["textDocument/codeAction"] = true,
-            },
-          },
-        },
-      }
+	{
+		"telescope.nvim",
+		dependencies = {
+			"gbrlsnchs/telescope-lsp-handlers.nvim",
+		},
+		opts = function()
+			local conf = require("plugins.configs.telescope")
+			local custom = {
+				file_ignore_patterns = { "/.git/", "^.git/", "^.clangd/", "/.clangd/", "^.cache/", "/.cache/" },
+				pickers = {
+					find_files = {
+						hidden = true,
+						follow = true,
+					},
+				},
+				extensions = {
+					lsp_handlers = {
+						disable = {
+							["textDocument/declaration"] = true,
+							["textDocument/definition"] = true,
+							["textDocument/implementation"] = true,
+							["textDocument/typeDefinition"] = true,
+							["textDocument/codeAction"] = true,
+						},
+					},
+				},
+			}
 
-      vim.tbl_deep_extend("error", conf, custom)
-      vim.list_extend(conf.extensions_list, { "lsp_handlers" })
+			vim.tbl_deep_extend("error", conf, custom)
+			vim.list_extend(conf.extensions_list, { "lsp_handlers" })
 
-      return conf
-    end,
-  },
+			return conf
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local cmp = require "cmp"
-      local conf = require "plugins.configs.cmp"
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function()
+			local cmp = require("cmp")
+			local conf = require("plugins.configs.cmp")
 
-      local custom = {
-        window = {
-          documentation = {
-            max_height = 0,
-          },
-        },
-        mapping = cmp.mapping.preset.insert {
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<Tab>"] = cmp.mapping.confirm { select = true },
-          ["<CR>"] = cmp.mapping.confirm { select = false },
-        },
-        experimental = {
-          ghost_text = true,
-        },
-      }
+			local custom = {
+				window = {
+					documentation = {
+						max_height = 0,
+					},
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-e>"] = cmp.mapping.abort(),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+				}),
+				experimental = {
+					ghost_text = true,
+				},
+			}
 
-      vim.tbl_deep_extend("force", conf, custom)
+			vim.tbl_deep_extend("force", conf, custom)
 
-      return conf
-    end,
-  },
+			return conf
+		end,
+	},
 
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function()
-      vim.g.rustfmt_autosave = 1
-    end,
-  },
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
 
-  {
-    "simrat39/rust-tools.nvim",
-    dependencies = "neovim/nvim-lspconfig",
-    ft = "rust",
-    config = function()
-      local lspconf = require "plugins.configs.lspconfig"
+	{
+		"simrat39/rust-tools.nvim",
+		dependencies = "neovim/nvim-lspconfig",
+		ft = "rust",
+		config = function()
+			local lspconf = require("plugins.configs.lspconfig")
 
-      local conf = {
-        tools = {
-          inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "type::",
-          },
-        },
-        server = {
-          on_attach = lspconf.on_attach,
-          capabilities = lspconf.capabilities,
-          settings = {
-            ["rust-analyzer"] = {
-              checkOnSave = { command = "clippy" },
-              diagnostics = { disabled = { "inactive-code" } },
-            },
-          },
-        },
-      }
+			local conf = {
+				tools = {
+					inlay_hints = {
+						show_parameter_hints = false,
+						parameter_hints_prefix = "",
+						other_hints_prefix = "type::",
+					},
+				},
+				server = {
+					on_attach = lspconf.on_attach,
+					capabilities = lspconf.capabilities,
+					settings = {
+						["rust-analyzer"] = {
+							checkOnSave = { command = "clippy" },
+							diagnostics = { disabled = { "inactive-code" } },
+						},
+					},
+				},
+			}
 
-      require("rust-tools").setup(conf)
-    end,
-  },
+			require("rust-tools").setup(conf)
+		end,
+	},
 
-  {
-    "chentoast/marks.nvim",
-    lazy = false,
-    opts = {
-      default_mappings = false,
-      signs = true,
-      mappings = {},
-    },
-  },
+	{
+		"chentoast/marks.nvim",
+		lazy = false,
+		opts = {
+			default_mappings = false,
+			signs = true,
+			mappings = {},
+		},
+	},
 
-  {
-    "ntpeters/vim-better-whitespace",
-    lazy = false,
-  },
+	{
+		"ntpeters/vim-better-whitespace",
+		lazy = false,
+	},
 
-  {
-    "tpope/vim-fugitive",
-    cmd = "Git",
-  },
+	{
+		"tpope/vim-fugitive",
+		cmd = "Git",
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    enabled = false,
-  },
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		enabled = false,
+	},
 }
 
 return plugins
